d7d075691a480bf3e7b230f566fcdc4c45f6e238
macdonad <macdonad@mail.gvsu.edu>
2013-10-03 12:38:47 -0400

    Reader

--- src/zebra-reader.c
+++ src/zebra-reader.c
@@ -22,13 +22,22 @@ int main(void) {
   char* readIn;
   int size = zebra_size;  
   key_t key = ftok(zebra_keygen, 0);
+  int shId;
+
+  //Get shared memory
+  if((shId = shmget(key, size, S_IRUSR|S_IWUSR)) < 0){
+    perror("Its not here.\n");
+    exit(1);
+  }
+
 
   //Attach to shared memory
-  if((sharedMem = shmat(key, 0, 0)) == (void*) -1){
+  if((sharedMem = shmat(shId, 0, 0)) == (void*) -1){
     perror("Didn't attach\n");
     exit(1);
   }
 
+  /*
   while(1){
     readIn = sharedMem;
     //Print out to display
@@ -45,7 +54,17 @@ int main(void) {
 
       exit(1);
     }
-   }
+   }*/
+  
+  //Detach
+  if(shmdt(sharedMem) < 0){
+    perror("detach failed.\n");
+    exit(1);
+  }
+  if(shmctl(shId, IPC_RMID, 0) < 0){
+    perror("deallocate failed.\n");
+    exit(1);
+  }
 
   return 0;
 }
