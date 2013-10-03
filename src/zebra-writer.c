4006715086f3da2e1ff8431cd452961b929ce3df
jordonbiondo <biondoj@mail.gvsu.edu>
2013-10-03 11:54:12 -0400

    create shared

--- src/zebra-writer.c
+++ src/zebra-writer.c
@@ -1,5 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
+#include "zebra-shared.h"
 
 int main(void) {
+  key_t mem_key = ftok(zebra_keygen, 'W');
   
+  int shmId = shmget(mem_key , zebra_size, IPC_CREAT|S_IWUSR);
+  printf("made: %d\n", shmId);
+  char* sharedPtr = shmat(shmId, 0, 0);
+
+  shmdt(sharedPtr);
+  
+  shmctl(shmId, IPC_RMID, 0);
+	     
+  return 0;
 }
