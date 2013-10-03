2f6cd21654f3283e8a8c035564a446554c76db3d
macdonad <macdonad@mail.gvsu.edu>
2013-10-03 12:16:25 -0400

    Changes to reader

--- src/zebra-reader.c
+++ src/zebra-reader.c
@@ -1,4 +1,51 @@
+/*
+ * Reader
+ *
+ *Jordon Biondo
+ *Doug MacDonald
+ *
+ * Reads from shared memory
+ * and outputs to display
+ */
 
+#include "zebra-shared.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <string.h>
 
 int main(void) {
+  char* sharedMem;
+  char* readIn;
+  int size = zebra_size;  
+  key_t key = ftok(zebra_keygen, 0);
+
+  //Attach to shared memory
+  if((sharedMem = shmat(key, 0, 0)) == (void*) -1){
+    perror("Didn't attach\n");
+    exit(1);
+  }
+
+  while(1){
+    readIn = sharedMem;
+    //Print out to display
+    printf("%s\n", readIn);
+
+
+    //read in exit
+    if(strcmp(readIn, "exit")){
+
+      //detach
+      if(shmdt(sharedMem) < 0){
+	  perror("detach failed\n");
+	}
+
+      exit(1);
+    }
+   }
+
+  return 0;
 }
