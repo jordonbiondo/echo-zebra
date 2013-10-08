0bdcde84059ca1e5213ffb15bfd7738ec98354bb
jordonbiondo <biondoj@mail.gvsu.edu>
2013-10-08 17:40:08 -0400

    writer

--- src/zebra-shared.h
+++ src/zebra-shared.h
@@ -2,9 +2,22 @@
 #ifndef __ZEBRA_SHARED_H__
 #define __ZEBRA_SHARED_H__
 
+typedef char byte;
+
+byte* int_bytes(int* x) {
+  return (byte*)x;
+}
+
+
+int bytes_to_int(byte* bytes) {
+  return bytes[0] 
+    + (bytes[1] << 8)
+    + (bytes[2] << 16)
+    + (bytes[3] << 24);
+}
+
 #define zebra_keygen "shared_file.txt"
 
 #define zebra_size 1024
-
 
 #endif
--- src/zebra-writer.c
+++ src/zebra-writer.c
@@ -2,21 +2,43 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-
+#include <string.h>
 #include "zebra-shared.h"
 
-int main(void) {
-  key_t mem_key = ftok(zebra_keygen, 'W');
-  
-  int shmId = shmget(mem_key , zebra_size, IPC_CREAT|S_IWUSR);
-  printf("made: %d\n", shmId);
-  char* sharedPtr = shmat(shmId, 0, 0);
 
-  shmdt(sharedPtr);
+/**
+ * Main
+ */
+int main(void) {
+
+  key_t mem_key = ftok(zebra_keygen, 0);
+
+  int shmId = shmget(mem_key , zebra_size, IPC_CREAT|S_IRUSR|S_IWUSR);
+  if (shmId < 0) {
+    printf("Failed get\n");
+    exit(-1);
+  }
+  
+  printf("made: %d\n", shmId);
+  printf("my pid: %d\n", getpid());
+  
+  char* sharedPtr = shmat(shmId, (void*)0, 0);
+  
+  if (sharedPtr < 0) {
+    printf("Failed attach\n");
+    exit(-1);
+  }
+  
+  pid_t my_pid = getpid();
+  
+  memcpy(sharedPtr, int_bytes(&my_pid), 4);
+  
+  sleep(10);
   
   shmctl(shmId, IPC_RMID, 0);
-	     
+
   return 0;
 }
